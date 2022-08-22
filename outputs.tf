06b7a5d2864043241dc13c0681e1e3b375059c86
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-22 20:42:10 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "oci-compartment" {
     value = local.this
     description = "Return an oci_identity_compartment resource or an empty string."
 }
+
+output "oci-group-admins" {
+    value = local.group_admins
+    description = "Return an oci_identity_group resource or an empty string."
+}
+
+output "oci-policy-admins" {
+    value = local.policy_admins
+    description = "Return an oci_identity_policy resource or an empty string."
+}
